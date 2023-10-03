13bd780143be852c8e8eee4e468001b5cd99f596
Bruno Strano <bruno_strano@hotmail.com>
2023-10-02 22:57:21 -0300

    ✨ Transaction create

--- src/modules/transactions/entities/transaction.interface.ts
+++ src/modules/transactions/entities/transaction.interface.ts
@@ -1,19 +1,19 @@
-import {IconsEnum} from "../../../shared/enums/icons.enum";
-import {ParticipantInterface} from "../../participants/entities/participant.interface";
-import {GroupInterface} from "../../groups/group.interface";
+import { IconsEnum } from '../../../shared/enums/icons.enum';
+import { ParticipantInterface } from '../../participants/entities/participant.interface';
+import { GroupInterface } from '../../groups/group.interface';
 
 export interface TransactionInterface {
-    id: string;
-    title: string;
-    description: string;
-    total: number;
-    icon: IconsEnum;
-    isSettled: boolean;
-    groupId: string;
-    ownedId: string;
-    creatorId: string;
+  id: string;
+  title: string;
+  description: string;
+  total: number;
+  icon: IconsEnum;
+  isSettled: boolean;
+  groupId: string;
+  ownedId: string;
+  creatorId: string;
 
-    owner?: ParticipantInterface;
-    creator?: ParticipantInterface;
-    group?: GroupInterface;
+  owner?: ParticipantInterface;
+  creator?: ParticipantInterface;
+  group?: GroupInterface;
 }
