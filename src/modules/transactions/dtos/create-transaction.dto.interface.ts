514ccbff9ab8fd4bbf04e413daefdf3ca7b091df
Gabriel Gondo <gabriel.gondo@hitcode.com.br>
2024-01-20 19:07:12 -0300

    ✨ make a transaction

--- src/modules/transactions/dtos/create-transaction.dto.interface.ts
+++ src/modules/transactions/dtos/create-transaction.dto.interface.ts
@@ -1,10 +1,15 @@
+import { IconsEnum } from "libs/united-sharedbill-core/src/shared/enums/icons.enum";
+import { TransactionTypeEnum } from "../enums/transaction-type.enum";
+
 export interface CreateTransactionDtoInterface {
   title: string;
   description: string;
+  icon: IconsEnum;
   total: number;
   groupId: string;
   owners: ParticipantAmountInterface[];
   debtors: ParticipantAmountInterface[];
+  transactionType: TransactionTypeEnum;
 }
 
 export interface ParticipantAmountInterface {
